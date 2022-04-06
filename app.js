f102501759ac529a72b47f933d4935b1b9a306b9
Levi Elliott <levihackworth@yahoo.com>
2022-04-06 11:49:40 -0500

    pseudocode for JS

---
+++ app.js
@@ -0,0 +1,17 @@
+// shuffle deck
+
+// players place bets
+
+// deal 2 cards face up to players
+
+// deal 1 face up and 1 face down to dealer
+
+//check for blackjack
+
+//player post deal choice
+// deal cards to players where applicable
+
+//check dealer for blackjack
+//deal cards to dealer
+
+//pay out based on results
