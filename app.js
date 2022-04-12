df2d23114dae3e394dfe496bbaf6a72329c0c249
Levi Elliott <levihackworth@yahoo.com>
2022-04-12 11:15:14 -0500

    added cites to js

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ shuffleDeck();
 startUp();
 
 // build and shuffle deck
+
+// cite https://w3collective.com/random-playing-card-javascript/ for help with card deck
 function buildDeck() {
      let amount = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
      let suite = ["C", "D", "H", "S"];
